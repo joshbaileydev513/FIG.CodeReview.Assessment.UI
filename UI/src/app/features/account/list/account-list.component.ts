105b06380eefbd7b7210b814e0f9db5bc4e07436
Joshua Bailey <joshbaileydev513@gmail.com>
2025-01-17 10:54:42 -0500

    added my own confirm delete modal for UX

--- UI/src/app/features/account/list/account-list.component.ts
+++ UI/src/app/features/account/list/account-list.component.ts
@@ -1,66 +1,82 @@
-import { IComponentOptions, IController } from 'angular';
-import { AccountService } from '../../../shared/services/account/account.service';
-import { AccountSummary } from '../../../shared/services/account/account-list-model';
+import { IComponentOptions, IController } from "angular";
+import { AccountService } from "../../../shared/services/account/account.service";
+import { AccountSummary } from "../../../shared/services/account/account-list-model";
 
 class AccountListController implements IController {
-    public accounts: AccountSummary[] = [];
-    public searchQuery = '';
-    public sortColumn = '';
-    public reverseSort = false;
+  public accounts: AccountSummary[] = [];
+  public searchQuery = "";
+  public sortColumn = "";
+  public reverseSort = false;
 
-    static $inject = ['accountService'];
+  public showDeleteModal = false;
+  public deleteTargetId?: number;
+  public deleteTargetLabel?: string;
 
-    constructor(private accountService: AccountService) { }
+  public deleteConfirmMessage = "Deleting this account cannot be undone.";
 
-    $onInit() {
-        this.accountService.getAllAccounts().then(data => {
-            this.accounts = data;
-        });
+  static $inject = ["accountService"];
+  constructor(private accountService: AccountService) {}
+
+  $onInit() {
+    this.accountService.getAllAccounts().then((data) => {
+      this.accounts = data;
+    });
+  }
+
+  public filteredAccounts(): AccountSummary[] {
+    if (!this.searchQuery) {
+      return this.accounts;
     }
 
-    // Filtering logic
-    public filteredAccounts(): AccountSummary[] {
-        if (!this.searchQuery) return this.accounts;
+    const lowerQuery = this.searchQuery.toLowerCase();
+    return this.accounts.filter((a) => {
+      return (
+        a.accountName.toLowerCase().includes(lowerQuery) ||
+        a.ownerName.toLowerCase().includes(lowerQuery) ||
+        a.createdDate.toLowerCase().includes(lowerQuery) ||
+        (a.checkingAmount + "").includes(lowerQuery) ||
+        (a.savingsAmount + "").includes(lowerQuery)
+      );
+    });
+  }
 
-        const lowerQuery = this.searchQuery.toLowerCase();
-        return this.accounts.filter(a => {
-            return (
-                a.accountName.toLowerCase().includes(lowerQuery) ||
-                a.ownerName.toLowerCase().includes(lowerQuery) ||
-                a.createdDate.toLowerCase().includes(lowerQuery) ||
-                (a.checkingAmount + '').includes(lowerQuery) ||
-                (a.savingsAmount + '').includes(lowerQuery)
-            );
-        });
+  public setSort(col: keyof AccountSummary) {
+    if (this.sortColumn === col) {
+      this.reverseSort = !this.reverseSort;
+    } else {
+      this.sortColumn = col;
+      this.reverseSort = false;
     }
+  }
 
-    // Sorting
-    public setSort(col: keyof AccountSummary) {
-        if (this.sortColumn === col) {
-            this.reverseSort = !this.reverseSort;
-        } else {
-            this.sortColumn = col;
-            this.reverseSort = false;
-        }
-    }
+  public openDeleteModal(account: AccountSummary) {
+    this.deleteTargetId = account.accountId;
+    this.deleteTargetLabel = `${account.ownerName} - ${account.accountName}`;
+    this.showDeleteModal = true;
+  }
 
-    public deleteAccount(id: number) {
+  public cancelDelete() {
+    this.showDeleteModal = false;
+    this.deleteTargetId = undefined;
+    this.deleteTargetLabel = undefined;
+  }
 
-        const sure = confirm("Are you sure you want to delete this account?");
-        if (!sure) return;
-
-        this.accountService.deleteAccount(id).then(() => {
-            return this.accountService.getAllAccounts();
-        }).then(data => {
-            this.accounts = data;
-        });
-    }
+  public confirmDelete() {
+    if (!this.deleteTargetId) return;
+    this.accountService
+      .deleteAccount(this.deleteTargetId)
+      .then(() => this.accountService.getAllAccounts())
+      .then((data) => {
+        this.accounts = data;
+        this.cancelDelete();
+      });
+  }
 }
 
-const template = require('./account-list.template.html');
+const template = require("./account-list.template.html");
 
 export const accountListComponent: IComponentOptions = {
-    bindings: {},
-    template,
-    controller: AccountListController
+  bindings: {},
+  template,
+  controller: AccountListController
 };
